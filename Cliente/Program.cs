27c51952c5739fbb12f3b0f7ddf5564b72168c3e
AdolfoPB <adolfo.pvw@gmail.com>
2022-05-18 23:19:57 -0400

    Agregar archivos de proyecto.

---
+++ Cliente/Program.cs
@@ -0,0 +1,59 @@
+﻿using ClienteHolaMundoS.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comunicaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            string servidor = ConfigurationManager.AppSettings["servidor"];
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Conectado a Servidor {0} en puerto {1}", servidor, puerto);
+            ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+            bool esValido = true;
+
+            if (clienteSocket.conectar())
+            {
+
+                string respuestaServidor;
+                string respuestaCliente;
+
+                Console.WriteLine("Conectado");
+
+                do
+                {
+
+                    respuestaCliente = Console.ReadLine();
+                    clienteSocket.Escribir(respuestaCliente);
+                    respuestaServidor = clienteSocket.Leer().Trim();
+                    Console.WriteLine("El servidor dice: {0}", respuestaServidor);
+                    respuestaCliente = Console.ReadLine();
+                    clienteSocket.Escribir(respuestaCliente);
+                    if (respuestaCliente == "chao")
+                    {
+                        Console.WriteLine("Desconectado");
+                        clienteSocket.Desconectar();
+                        esValido = false;
+                    }
+
+
+                } while (esValido);
+            }
+            else
+            {
+                Console.WriteLine("Error de comunicacion");
+            }
+
+        }
+    }
+}
---
+++ EjercicioSocket/Program.cs
@@ -0,0 +1,58 @@
+﻿using ServerSocketBib;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSocket
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            ServerSocket servidor = new ServerSocket(puerto);
+
+            if (servidor.Iniciar())
+            {
+                //ok puede iniciar
+                Console.WriteLine("Servidor Iniciado");
+                while (true)
+                {
+                    Console.WriteLine("Esperando Cliente");
+                    Socket socketCliente = servidor.ObtenerCliente();
+                    //construir el mecanismo para escribirle y leerle 
+                    ClienteCom cliente = new ClienteCom(socketCliente);
+                    //aqui esta el protocolo de comunicacion, que ambos saben
+
+                    string respuestaCliente;
+                    string respuestaServidor;
+                    bool esValido = true;
+                    do
+                    {
+                        respuestaCliente = cliente.Leer().Trim();
+                        Console.WriteLine("El cliente dice: {0}", respuestaCliente);
+                        respuestaServidor = Console.ReadLine();
+                        cliente.Escribir(respuestaServidor);
+
+                        if (respuestaServidor == "chao")
+                        {
+                            cliente.Desconectar();
+                            esValido = false;
+                        }
+                        
+
+                    } while (esValido);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error, el puerto {0} esta en uso", puerto);
+            }
+        }
+    }
+}
